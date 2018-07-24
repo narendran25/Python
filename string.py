b14f08ea30b9aa0c08e17fc016ee5e52479f9033
Narendran C <40605746+narendran25@users.noreply.github.com>
2018-07-24 08:48:52 -0700

    Add files via upload

---
+++ string.py
@@ -0,0 +1,13 @@
+string=raw_input("Enter the string:")
+count1=0
+count2=0
+for i in string:
+      if(i.islower()):
+            count1=count1+1
+      elif(i.isupper()):
+            count2=count2+1
+
+print("The number of lowercase characters is:")
+print(count1)
+print("The number of uppercase characters is:")
+print(count2)
---
+++ swap.py
@@ -0,0 +1,7 @@
+x=input("Enter x value")
+y=input("Enter y value")
+
+x,y=y,x
+
+print("x=",x)
+print("y=",y)
